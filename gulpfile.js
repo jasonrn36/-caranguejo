e24ec3088d6fd247365136512a8e37923a07273c
jsonrn36 <jason.rn36@gmail.com>
2026-02-19 11:02:47 -0400

    criado todas as pastas e documentos padrão para o inicio dos trabalhos

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function compilarSass() {
+    return src('src/estilos/**/*.scss') // seleciona todos os arquivos SCSS na pasta src/estilos
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(dest('dist/css'));
+    console.log("Ok suas Pastas foram compiladas \n de scss para css.");
+}
+
+function copiarHTML() { // funcão para copiar arquivos HTML
+    return src('src/*.html') // seleciona todos os arquivos HTML na pasta src
+    .pipe(dest('dist')); // copia os arquivos HTML para a pasta dist
+    console.log("Ok suas Pastas foram copiadas \n de src para dist."); // mensagem de sucesso
+}
+
+// função para observar mudanças nos arquivos
+function observar() { 
+watch('src/scss/**/*.scss', compilarSass, copiarHTML);
+    console.log("Observando tarefas de compilação de Sass...");
+}
+
+// tarefa para compilar Sass e copiar HTML
+exports.build = series(compilarSass, copiarHTML); 
+exports.default = observar;
