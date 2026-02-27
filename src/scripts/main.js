41024a7278c1673daf78a570079d92dd4a4acedd
jsonrn36 <jason.rn36@gmail.com>
2026-02-27 15:42:09 -0400

    colocado o timeStamp no projeto

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
 AOS.init();
+
+//declaration of variables
+const dataDoEvento = new Date("Jun 6, 2026 20:00:00"); // Declaration date in the future
+const timeStampDoEvento = dataDoEvento.getTime(); //Declaration of get DateTime of the constant will declarate on above
+
+const contaAsHoras = setInterval(function () {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor(distanciaAteOEvento % diaEmMs) / (horaEmMs);
+    const minutosAteOEvento = Math.floor ((distanciaAteOEvento % horaEmMs) / minutosEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutos) / 1000);
+
+
+    console.log(diasAteOEvento);
+    console.log(horasAteOEvento);
+    console.log(minutosAteOEvento);
+    console.log(segundosAteOEvento)
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+}, 1000);
