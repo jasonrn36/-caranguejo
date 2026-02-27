6fd259284b7f8a66be20d181d64ede4083eb904e
jsonrn36 <jason.rn36@gmail.com>
2026-02-27 16:13:59 -0400

    finalizado o tipme stamp dos dias do evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 AOS.init();
 
 //declaration of variables
-const dataDoEvento = new Date("Jun 6, 2026 20:00:00"); // Declaration date in the future
+const dataDoEvento = new Date("Jun 6, 2026 23:00:00"); // Declaration date in the future
 const timeStampDoEvento = dataDoEvento.getTime(); //Declaration of get DateTime of the constant will declarate on above
 
 const contaAsHoras = setInterval(function () {
@@ -10,14 +10,20 @@ const contaAsHoras = setInterval(function () {
 
     const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
 
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById("contador").innerHTML = "O evento Expirou";
+        return;
+    }
+
     const diaEmMs = 1000 * 60 * 60 * 24;
     const horaEmMs = 1000 * 60 * 60;
     const minutosEmMs = 1000 * 60;
 
     const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
-    const horasAteOEvento = Math.floor(distanciaAteOEvento % diaEmMs) / (horaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
     const minutosAteOEvento = Math.floor ((distanciaAteOEvento % horaEmMs) / minutosEmMs);
-    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutos) / 1000);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / 1000);
 
 
     console.log(diasAteOEvento);
@@ -25,5 +31,9 @@ const contaAsHoras = setInterval(function () {
     console.log(minutosAteOEvento);
     console.log(segundosAteOEvento)
 
-    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+
+
+
 }, 1000);
